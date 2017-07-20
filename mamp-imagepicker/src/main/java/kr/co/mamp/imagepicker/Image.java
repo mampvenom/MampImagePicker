82ee8b094c4e14a1c4fec6db60c61ea81d910c84
mampvenom <mampvenom@gmail.com>
2017-07-20 17:37:12 +0900

    이미지 객체 수정.

--- mamp-imagepicker/src/main/java/kr/co/mamp/imagepicker/Image.java
+++ mamp-imagepicker/src/main/java/kr/co/mamp/imagepicker/Image.java
@@ -1,9 +1,16 @@
 package kr.co.mamp.imagepicker;
 
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import kr.co.mamp.imagepicker.loader.ImageLoader;
 
 public class Image {
 
@@ -50,6 +57,41 @@ public class Image {
         return "original: " + original.toString() +
                 "\nthumbnail: " + thumbnail.toString() +
                 "\norientation: " + orientation;
+    }
+
+
+    public void addToThumbnailView(Context context, ImageView thumbnailView) {
+        addToImageView(context, thumbnailView, thumbnail);
+    }
+
+
+    public void addToOriginalView(Context context, ImageView originalView) {
+        addToImageView(context, originalView, original);
+    }
+
+
+    private void addToImageView(Context context, ImageView imageView, Uri uri) {
+        if (uri != null) {
+            if (orientation == 0) {
+                imageView.setImageURI(uri);
+            } else if (orientation == 180) {
+                imageView.setImageURI(uri);
+                imageView.setRotation(180);
+            } else {
+                Matrix matrix = new Matrix();
+                matrix.setRotate(orientation);
+
+                String realPath = new ImageLoader.ImageCursor(context).getRealPathFromUri(uri);
+                Bitmap bitmap = BitmapFactory.decodeFile(realPath);
+                if (bitmap != null) {
+                    Bitmap oriented = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                    bitmap.recycle();
+                    imageView.setImageBitmap(oriented);
+                } else {
+                    imageView.setImageURI(uri);
+                }
+            }
+        }
     }
 
 }
